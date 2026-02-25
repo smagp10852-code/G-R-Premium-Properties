b8f8c27aede160801c1c1337ebd9cd07fb2c5f04
Umesh Verma <rajumesh8969457707@gmail.com>
2026-02-25 18:35:18 +0530

    Updated contact page layout, icons, and full-width map

--- sanity/authproject/gr-authproject/static/sanity-lTCbmu1M.js
+++
@@ -1,1 +0,0 @@
-import{s as t}from"./sanity.config-CLa_3N7v.js";import{renderStudio as o}from"sanity";import"sanity/structure";import"@sanity/vision";o(document.getElementById("sanity"),t,{reactStrictMode:!1,basePath:"/"});
